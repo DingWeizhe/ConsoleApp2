ea1453aba0a1c3dc28b3a0c56527aa292f340f69
dweiz <dweiz@DESKTOP-FD273FH>
2021-06-18 20:25:16 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://api.gosu.bar/status");
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+            Console.ReadLine();
+        }
+    }
+}
